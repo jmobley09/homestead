b571345d4463b6198e4de5cbdc75984ccf6a3c7e
joshua.mobley <joshua.mobley@simplethread.com>
2022-11-29 15:16:41 -0500

    backend fastapi setup with initial endpoints and sqlite connection

---
+++ server/sql/schemas.py
@@ -0,0 +1,36 @@
+# schemas is synonym for pydantic models
+from typing import Union
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    name: str
+    description: Union[str, None] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class CategoryBase(BaseModel):
+    name: str
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class Category(CategoryBase):
+    id: int
+    items: list[Item] = []
+
+    class Config:
+        orm_mode = True
