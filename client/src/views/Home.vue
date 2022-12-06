1b45ae2c5a484e9c2de81a6ec5c6999f672eb0e6
joshua.mobley <joshua.mobley@simplethread.com>
2022-12-06 13:36:25 -0500

    adding icon and formatting page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="d-flex flex-column">
+    <v-img src="public/icon.png" max-height="150" max-width="150" class="mt-16"></v-img>
     <h1>Welcome to the Homestead App</h1>
     <p>Please select an option from the left to get started.</p>
   </div>
