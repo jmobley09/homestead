385164caa6245381facc3df4b01ed12b710b45ff
joshua.mobley <joshua.mobley@simplethread.com>
2022-12-06 12:20:49 -0500

    moving dependencies to own submodule

--- server/routers/categories.py
+++ server/routers/categories.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from sql.database import SessionLocal
-from sql import crud, models, schemas
+from sql import schemas
+from dependencies import categories_crud
 
 router = APIRouter(
     prefix="/categories",
@@ -19,19 +20,19 @@ def get_db():
 
 @router.post("/", response_model=schemas.Category)
 def create_category(category: schemas.CategoryCreate, db: Session = Depends(get_db)):
-    db_category = crud.get_category_by_name(db, name=category.name)
+    db_category = categories_crud.get_category_by_name(db, name=category.name)
     if db_category:
         raise HTTPException(status_code=400, detail="Category already exists")
-    return crud.create_category(db=db, category=category)
+    return categories_crud.create_category(db=db, category=category)
 
 @router.get("/", response_model=list[schemas.Category])
 def read_categories(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    categories = crud.get_categories(db, skip=skip, limit=limit)
+    categories = categories_crud.get_categories(db, skip=skip, limit=limit)
     return categories
 
 @router.get("/{category_id}", response_model=schemas.Category)
 def read_category(category_id: int, db: Session = Depends(get_db)):
-    db_category = crud.get_category(db, category_id=category_id)
+    db_category = categories_crud.get_category(db, category_id=category_id)
     if db_category is None:
         raise HTTPException(status_code=404, detail="Category not found")
     return db_category
@@ -40,4 +41,4 @@ def read_category(category_id: int, db: Session = Depends(get_db)):
 def create_item_for_category(
     category_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
-    return crud.create_category_item(db=db, item=item, category_id=category_id)
+    return categories_crud.create_category_item(db=db, item=item, category_id=category_id)
